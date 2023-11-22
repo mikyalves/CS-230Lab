0103c6f3f96d76e8520088adfdd3b750cd178d12
mikyalves <msa0043@mix.wvu.edu>
2023-11-22 01:03:45 -0500

    Lab 06

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Card from './Card';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>CS 230L</h1>
+    <div className="App" style={{fontFamily: 'Times New Roman, Times, serif' }}>
+      <Navbar />
+
+      <h1>CS 230Lab</h1>
       <h2>Section -003</h2>
       <p>WVU ID: 800301530</p>
       <p>Hi, I'm MiKy!</p>
+
+      <div className="container">
+        <Card />
+      </div>
     </div>
   );
 }
